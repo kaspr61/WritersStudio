9b42b3934d798f318705885586ad083ba8718c25
Repsakras <kasper.s.skott@gmail.com>
2020-03-26 18:34:59 +0100

    Finalized v0.1 to showcase at retrospective 1.

--- src/main/java/com/team34/App.java
+++ src/main/java/com/team34/App.java
@@ -1,26 +1,20 @@
 package com.team34;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-/**
- * JavaFX App
- */
 public class App extends Application {
 
-
     @Override
-    public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/main.fxml"));
-        Scene scene = new Scene(root, 600, 400);
+    public void start(Stage stage) {
+        VBox vBox = new VBox();
+        vBox.getStylesheets().add(getClass().getResource("css/main.css").toExternalForm());
+
+        Scene scene = new Scene(vBox, 1280, 720);
 
         stage.setTitle("Writer's Studio");
-        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
