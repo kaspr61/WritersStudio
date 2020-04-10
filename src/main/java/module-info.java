79816326b4b19fa4a9927643b0c8a316ce07e7af
yamaz420 <erikperik_johnsson@hotmail.com>
2020-04-10 13:19:54 +0200

    layout
    hej

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
-module com.team34 {
+/**module com.team34 {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.team34 to javafx.fxml;
+
+    opens com.team34. to. javafx.fxml;
     exports com.team34;
 }
+
+ */
