733c65d058d69eead08e5d2ecb47f8b0bf052a46
jimanderssongit <62330362+jimanderssongit@users.noreply.github.com>
2020-04-30 15:56:08 +0200

    Update of character list
    * Added new stylesheet for the class
    * Fixed icons

--- src/main/java/com/team34/CharacterList.java
+++ src/main/java/com/team34/CharacterList.java
@@ -35,6 +35,9 @@ public class CharacterList extends Application {
 
     //Icons
     private String addCharacter;
+    private String editCharacter;
+    private String deleteCharacter;
+    private final int ICON_SIZE = 40;
 
     public static void main(String[] args) {
         launch(args);
@@ -50,15 +53,9 @@ public class CharacterList extends Application {
         BorderPane innerPane = new BorderPane();
 
         //CSS
-        cssMain = com.team34.App.class.getResource("css/main.css").toExternalForm();
+        cssMain = com.team34.App.class.getResource("css/characterlist.css").toExternalForm();
         scene = new Scene(outerPane, 300, 500);
         scene.getStylesheets().add(cssMain);
-
-        //Icon TODO: Hur fan får jag kompilatorn att inte ge mig null pointers för bilden?
-        addCharacter = com.team34.App.class.getResource("icons/add_character.png").toExternalForm();
-        FileInputStream inputAddCharacter = new FileInputStream(addCharacter);
-        Image imgAddCharacter = new Image(inputAddCharacter);
-        ImageView imageViewAddCharacter = new ImageView(imgAddCharacter);
 
         //Add, Edit, Delete buttons
         HBox aedBox = new HBox();
@@ -77,17 +74,46 @@ public class CharacterList extends Application {
 
         //Buttons
         add = new Button();
-        edit = new Button("Edit");
-        delete = new Button("Delete");
+        edit = new Button();
+        delete = new Button();
         save = new Button("Save");
         cancel = new Button("Cancel");
 
-        //Icons for Add/Edit/Delete buttons
+        save.getStyleClass().add("button-save-cancel"); //TODO: Fixa i stylesheet så att save och cancel behåller default
+        cancel.getStyleClass().add("button-save-cancel");
+
+        //Icons for Add/Edit/Delete
+        addCharacter = com.team34.App.class.getResource("icons/add_character.png").getPath(); //Filestream for icon
+        FileInputStream inputAddCharacter = new FileInputStream(addCharacter);
+        Image imgAddCharacter = new Image(inputAddCharacter);
+        ImageView imageViewAddCharacter = new ImageView(imgAddCharacter);
+
+        editCharacter = com.team34.App.class.getResource("icons/edit_character.png").getPath();
+        FileInputStream inputEditCharacter = new FileInputStream(editCharacter);
+        Image imgEditCharacter = new Image(inputEditCharacter);
+        ImageView imageViewEditCharacter = new ImageView(imgEditCharacter);
+
+        deleteCharacter = com.team34.App.class.getResource("icons/delete_character.png").getPath();
+        FileInputStream inputDeleteCharacter = new FileInputStream(deleteCharacter);
+        Image imgDeleteCharacter = new Image(inputDeleteCharacter);
+        ImageView imageViewDeleteCharacter = new ImageView(imgDeleteCharacter);
+
+        imageViewAddCharacter.setFitHeight(ICON_SIZE); // Set size for icon
+        imageViewAddCharacter.setFitWidth(ICON_SIZE);
+
+        imageViewEditCharacter.setFitHeight(ICON_SIZE);
+        imageViewEditCharacter.setFitWidth(ICON_SIZE);
+
+        imageViewDeleteCharacter.setFitHeight(ICON_SIZE);
+        imageViewDeleteCharacter.setFitWidth(ICON_SIZE);
+
         add.setGraphic(imageViewAddCharacter);
+        edit.setGraphic(imageViewEditCharacter);
+        delete.setGraphic(imageViewDeleteCharacter);
 
         //Label
         title = new Label("Characters");
-        title.getStyleClass().add("character-list-headline");
+        title.setPadding(new Insets(20, 0, 0, 0));
 
         //Character List
         ArrayList<String> characters = new ArrayList<>();
