8ea51675f8eb6d3a3e889a42bd79973f5ccf3741
Repsakras <62218681+Repsakras@users.noreply.github.com>
2020-05-18 19:37:45 +0200

    Moving blocks and attaching associations now work. Added snapping to both.

--- src/main/java/com/team34/view/characterchart/CharacterChart.java
+++ src/main/java/com/team34/view/characterchart/CharacterChart.java
@@ -5,8 +5,10 @@ import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tooltip;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.input.*;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
@@ -23,17 +25,23 @@ public class CharacterChart {
     private HashMap<Long, CharacterRectangle> rectMap; // Stores references to CharacterRectangles by their UID.
     private HashMap<Long, AssociationPoint> assocPoints;
     private HashMap<Long, Association> associations;
+    private long lastAssocPtClicked;
+    private AssociationPoint originalAssocPtClicked;
+    private Rectangle currentRectDragOver;
 
     private final EventHandler<MouseEvent> evtRectPressed;
     private final EventHandler<MouseEvent> evtRectDragged;
+    private final EventHandler<MouseEvent> evtRectReleased;
 
     public CharacterChart(double width, double height) {
         rectMap = new HashMap<>();
         assocPoints = new HashMap<>();
         associations = new HashMap<>();
+        lastAssocPtClicked = -1L;
 
         evtRectPressed = new EventRectanglePressed();
         evtRectDragged = new EventRectangleDragged();
+        evtRectReleased = new EventRectangleReleased();
 
         pane = new Pane();
 //        pane.setMinSize(width, height);
@@ -80,6 +88,10 @@ public class CharacterChart {
 
         rect.getRect().setOnMousePressed(evtRectPressed);
         rect.getRect().setOnMouseDragged(evtRectDragged);
+        rect.getRect().setOnMouseReleased(evtRectReleased);
+        rect.getRect().setOnMouseDragEntered(evtRectMouseDragEntered);
+        rect.getRect().setOnMouseDragExited(evtRectMouseDragExited);
+        rect.getRect().setOnMouseDragReleased(evtRectMouseDragReleased);
 
     }
 
@@ -98,11 +110,28 @@ public class CharacterChart {
         return null;
     }
 
+    private Map.Entry<Long, AssociationPoint> getAssocPointByControl(Circle circle) {
+        Iterator<Map.Entry<Long, AssociationPoint>> it = assocPoints.entrySet().iterator();
+        Map.Entry<Long, AssociationPoint> pair;
+
+        // Find AssociationPoint that contains the input circle and return its associated UID
+        while (it.hasNext()) {
+            pair = it.next();
+            if(pair.getValue().control.equals(circle)) {
+                return pair;
+            }
+        }
+
+        return null;
+    }
+
     public void addAssociation(long assocUID, long startPtUID, long endPtUID, long startCharUID, long endCharUID, String label)
     {
         Association exisitingAssoc = associations.get(assocUID);
         if(exisitingAssoc != null) {
-            pane.getChildren().removeAll(exisitingAssoc.text, exisitingAssoc.line);
+            AssociationPoint start = assocPoints.get(startPtUID);
+            AssociationPoint end = assocPoints.get(endPtUID);
+            pane.getChildren().removeAll(exisitingAssoc.text, exisitingAssoc.line, start.control, end.control);
         }
 
         AssociationPoint startPt = new AssociationPoint(false);
@@ -114,22 +143,38 @@ public class CharacterChart {
         assoc.text.getStyleClass().add("characterchart-assoclabel");
         assoc.line = new Line(0, 0, 0, 0);
         assoc.line.getStyleClass().add("characterchart-assocline");
+        assoc.line.setViewOrder(-1.5);
+        assoc.line.setMouseTransparent(true);
 
         startPt.assocUID = assocUID;
         startPt.rectUID = startCharUID;
         startPt.x = 0;
         startPt.y = 0;
+        startPt.control = new Circle(8.0);
+        startPt.control.setViewOrder(-2.0);
+        startPt.control.getStyleClass().add("characterchart-line-control");
+        startPt.control.setOnMousePressed(evtAPMousePressed);
+        startPt.control.setOnMouseReleased(evtAPMouseReleased);
+        startPt.control.setOnDragDetected(evtAPDragDetected);
+        startPt.control.setOnMouseDragged(evtAPMouseDragged);
 
         endPt.assocUID = assocUID;
         endPt.rectUID = endCharUID;
         endPt.x = 0;
         endPt.y = 0;
+        endPt.control = new Circle(8.0);
+        endPt.control.setViewOrder(-2.0);
+        endPt.control.getStyleClass().add("characterchart-line-control");
+        endPt.control.setOnMousePressed(evtAPMousePressed);
+        endPt.control.setOnMouseReleased(evtAPMouseReleased);
+        endPt.control.setOnDragDetected(evtAPDragDetected);
+        endPt.control.setOnMouseDragged(evtAPMouseDragged);
 
         associations.put(assocUID, assoc);
         assocPoints.put(startPtUID, startPt);
         assocPoints.put(endPtUID, endPt);
 
-        pane.getChildren().addAll(assoc.line, assoc.text);
+        pane.getChildren().addAll(assoc.line, assoc.text, startPt.control, endPt.control);
 
         if(startCharUID != -1L)
             rectMap.get(startCharUID).addAssociationPoint(startPtUID);
@@ -147,11 +192,19 @@ public class CharacterChart {
     }
 
     public void attachAssociationPointToCharacter(long assocPtUID, long characterUID) {
+        if(assocPtUID == -1L || characterUID == -1L)
+            return;
+
         rectMap.get(characterUID).addAssociationPoint(assocPtUID);
+        assocPoints.get(assocPtUID).rectUID = characterUID;
     }
 
     public void detachAssociationPointFromCharacter(long assocPtUID, long characterUID) {
+        if(assocPtUID == -1L || characterUID == -1L)
+            return;
+
         rectMap.get(characterUID).removeAssociationPoint(assocPtUID);
+        assocPoints.get(assocPtUID).rectUID = -1L;
     }
 
     public void setCharacterPosition(long uid, double x, double y) {
@@ -181,6 +234,9 @@ public class CharacterChart {
     private void updateAssociationLinePoint(AssociationPoint point) {
         Line line = associations.get(point.assocUID).line;
 
+        point.control.setCenterX(point.x);
+        point.control.setCenterY(point.y);
+
         if(point.end) {
             line.setEndX(point.x);
             line.setEndY(point.y);
@@ -190,6 +246,125 @@ public class CharacterChart {
             line.setStartY(point.y);
         }
     }
+
+    /********************* EVENT LAMBDAS ***********************/
+
+    private EventHandler<MouseEvent> evtAPMouseDragged = e -> {
+        if(lastAssocPtClicked == -1L)
+            return;
+
+        double x = e.getX();
+        double y = e.getY();
+        if(currentRectDragOver != null) {
+            boolean invalidX = false, invalidY = false;
+
+            if(x < currentRectDragOver.getX() + currentRectDragOver.getWidth() * 0.2) // snap vertically
+                x = currentRectDragOver.getX();
+            else if(x > currentRectDragOver.getX() + currentRectDragOver.getWidth() * 0.8)
+                x = currentRectDragOver.getX() + currentRectDragOver.getWidth();
+            else
+                invalidX = true;
+
+            if(y < currentRectDragOver.getY() + currentRectDragOver.getHeight() * 0.2) // snap horizontally
+                y = currentRectDragOver.getY();
+            else if(y > currentRectDragOver.getY() + currentRectDragOver.getHeight() * 0.8)
+                y = currentRectDragOver.getY() + currentRectDragOver.getHeight();
+            else
+                invalidY = true;
+
+            // If mouse is in the center, snap to nearest edge
+            if(invalidX && invalidY) {
+                double centerX = currentRectDragOver.getX() + currentRectDragOver.getWidth() * 0.5;
+                double centerY = currentRectDragOver.getY() + currentRectDragOver.getHeight() * 0.5;
+
+                // Snap to left edge
+                if(x < centerX && y > currentRectDragOver.getY() + currentRectDragOver.getHeight() * 0.33 &&
+                        y < currentRectDragOver.getY() + currentRectDragOver.getHeight() * 0.67) {
+                    x = currentRectDragOver.getX();
+                    y = centerY;
+                } // Snap to right edge
+                else if(x >= centerX && y > currentRectDragOver.getY() + currentRectDragOver.getHeight() * 0.33 &&
+                        y < currentRectDragOver.getY() + currentRectDragOver.getHeight() * 0.67) {
+                    x = currentRectDragOver.getX() + currentRectDragOver.getWidth();
+                    y = centerY;
+                } // Snap to upper edge
+                else if(y < centerY) {
+                    x = centerX;
+                    y = currentRectDragOver.getY();
+                } // Snap to bottom edge
+                else if(y >= centerY) {
+                    x = centerX;
+                    y = currentRectDragOver.getY() + currentRectDragOver.getHeight();
+                }
+
+            }
+        }
+
+        setAssociationPointPosition(lastAssocPtClicked, x, y);
+    };
+
+    private EventHandler<MouseEvent> evtAPDragDetected = e -> {
+        ((Circle)e.getSource()).startFullDrag();
+        e.consume();
+    };
+
+    private EventHandler<MouseEvent> evtAPMouseReleased = e -> {
+        Circle src = (Circle) e.getSource();
+
+        if(originalAssocPtClicked != null) { // Reset to original point
+            AssociationPoint pt = assocPoints.get(lastAssocPtClicked);
+            pt.x = originalAssocPtClicked.x;
+            pt.y = originalAssocPtClicked.y;
+            updateAssociationLinePoint(pt);
+            originalAssocPtClicked = null;
+        }
+
+        lastAssocPtClicked = -1L;
+        currentRectDragOver = null;
+        src.setMouseTransparent(false);
+    };
+
+    private EventHandler<MouseEvent> evtAPMousePressed = e -> {
+        Circle src = (Circle) e.getSource();
+        Map.Entry<Long, AssociationPoint> assocPt = getAssocPointByControl(src);
+        lastAssocPtClicked = assocPt.getKey();
+        src.setMouseTransparent(true);
+
+        originalAssocPtClicked = new AssociationPoint(assocPt.getValue().end);
+        originalAssocPtClicked.x = assocPt.getValue().x;
+        originalAssocPtClicked.y = assocPt.getValue().y;
+    };
+
+    private EventHandler<MouseDragEvent> evtRectMouseDragReleased = e -> {
+        Circle circle = (Circle) e.getGestureSource();
+        Map.Entry<Long, AssociationPoint> assocPt = getAssocPointByControl(circle);
+        Rectangle rectangle = (Rectangle) e.getSource();
+        Map.Entry<Long, CharacterRectangle> character = getCharacterByRectangle(rectangle);
+
+        if(assocPt.getValue().rectUID != character.getKey()) { // assoc point dropped onto new character
+            if(assocPt.getValue().rectUID != -1L)
+                detachAssociationPointFromCharacter(assocPt.getKey(), assocPt.getValue().rectUID);
+
+            attachAssociationPointToCharacter(assocPt.getKey(), character.getKey());
+        }
+
+        originalAssocPtClicked = null; // Drop was valid, don't reset to original position.
+        e.consume();
+    };
+
+    private EventHandler<MouseDragEvent> evtRectMouseDragEntered = e -> {
+        System.out.println("onMouseDragEntered");
+        currentRectDragOver = (Rectangle) e.getSource();
+        e.consume();
+    };
+
+    private EventHandler<MouseDragEvent> evtRectMouseDragExited = e -> {
+        System.out.println("onMouseDragExited");
+        currentRectDragOver = null;
+        e.consume();
+    };
+
+    /********************** EVENT CLASSES ***********************/
 
     private class EventRectanglePressed implements EventHandler<MouseEvent> {
         @Override
@@ -209,11 +384,29 @@ public class CharacterChart {
             if(character != null) {
                 double x = e.getX() - character.getValue().getLastClickX();
                 double y = e.getY() - character.getValue().getLastClickY();
+
+                // Snap to nearest 10
+                x = (double)(((int)x / 10) * 10);
+                y = (double)(((int)y / 10) * 10);
+
                 setCharacterPosition(character.getKey(), x, y);
+                rectangle.setViewOrder(-1.0);
+                character.getValue().getText().setViewOrder(-1.0);
             }
         }
     }
 
+    private class EventRectangleReleased implements EventHandler<MouseEvent> {
+        @Override
+        public void handle(MouseEvent e) {
+            Rectangle rectangle = (Rectangle) e.getSource();
+            Map.Entry<Long, CharacterRectangle> character = getCharacterByRectangle(rectangle);
+            if(character != null) {
+                rectangle.setViewOrder(0.0);
+                character.getValue().getText().setViewOrder(0.0);
+            }
+        }
+    }
 }
 
 class Association {
@@ -227,6 +420,7 @@ class AssociationPoint {
     public long rectUID; // UID to which rect the point is attached to.
     public double x, y;
     public final boolean end;
+    public Circle control;
 
     AssociationPoint(boolean isEndPoint) {
         assocUID = -1L;
